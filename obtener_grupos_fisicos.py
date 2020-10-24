06ac8b3f40004d9c10f32614fc4a73fc1c8b5505
Alejandro Hincapie G <alejohg03@gmail.com>
2020-10-23 19:59:36 -0500

    Agregando función

--- obtener_grupos_fisicos.py
+++ obtener_grupos_fisicos.py
@@ -20,8 +20,8 @@ def grupos_fisicos(archivo, dim=-1):
         de tal dimensión, si no, se reportan todos.
         Retorna:
             - Un diccionario en el cual las claves son las ETIQUETAS de los 
-              grupos físicos y los valores son los NOMBRES asociados a cada
-              etiqueta.
+              grupos físicos y los valores son tuplas con la DIMENSIÓN y el
+              NOMBRE asociados a cada etiqueta.
             - Un diccionario en el cual las claves son las etiquetas de los
               grupos físicos y los valores son listas con los nodos asociados
               a cada grupo físico.
@@ -30,34 +30,31 @@ def grupos_fisicos(archivo, dim=-1):
         raise ValueError('Solo se admite un archivo de extensión .msh')
 
     gmsh.initialize()
-
     gmsh.open(archivo)
+
     grupos = gmsh.model.getPhysicalGroups(dim)
     dims = [d[0] for d in grupos]
     tags = [d[1] for d in grupos]
-    nombres = [gmsh.model.getPhysicalName(dim, tag) for dim, tag in grupos]
+    nombres = [(dim, gmsh.model.getPhysicalName(dim, tag))
+                for dim, tag in grupos]
 
     dict1 = dict(zip(tags, nombres))  # Primer diccionario a reportar
 
-    dim_nodos = [(dims[i], gmsh.model.mesh.getNodesForPhysicalGroup(dims[i],
-                                                                    tags[i])[0])
-                 for i in range(len(dims))]
-    dict2 = dict(zip(tags, dim_nodos))  # Segundo diccionario a reportar
+    nodos = [gmsh.model.mesh.getNodesForPhysicalGroup(dims[i], tags[i])[0] for i in range(len(dims))]
+    dict2 = dict(zip(tags, nodos))  # Segundo diccionario a reportar
 
     gmsh.finalize()
 
     return dict1, dict2
 
-# %% Ejemplo de uso:
 
-file = 'scordelis.msh'
-
-d1, d2 = grupos_fisicos(file)
-
-print('Grupos físicos reportados:\n')
-for tag in d1.keys():
-    nombre = d1[tag]
-    dim    = d2[tag][0]
-    nodos  = d2[tag][1]
-    print(f'Grupo físico: {tag}\nDimensión: {dim}\nNombre: {nombre}\n'
-          f'Contiene nodos: {nodos}.\n')
+def obtener_nodos(archivo, nombre_grupo):
+    ''' Lee un archivo de extensión .msh y obtiene los nodos asociados al grupo físico de nombre "nombre_grupo".
+    '''
+    d1, d2 = grupos_fisicos(archivo)
+    nombres = [t[1] for t in d1.values()]
+    if nombre_grupo in nombres:
+        nodos = d2[d2.keys()[nombres.index(nombre_grupo)]]
+        return nodos
+    else:
+        raise ValueError('El nombre de grupo físico ingresado no existe.')
