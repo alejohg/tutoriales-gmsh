7fed55239033ced1dfb74337a9fd899fe9da7b5d
Alejandro Hincapie G <alejohg03@gmail.com>
2020-10-23 20:24:37 -0500

    Arreglando función

--- obtener_grupos_fisicos.py
+++ obtener_grupos_fisicos.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Función para obtener todos los grupos físicos definidos en una malla, su dimen-
+Funciones para obtener todos los grupos físicos definidos en una malla, su dimen-
 sión y los nodos asociados a cada uno, a partir de un archivo .msh exportado
 por GMSH.
 
-Para correr este programa y usar la función se requiere tener instalada la API
+Para correr este programa y usar las funciones se requiere tener instalada la API
 de GMSH en Python, usar el comando pip install --upgrade gmsh
 
 Por: Alejandro Hincapié G.
 """
 
 import gmsh  # pip install --upgrade gmsh
-import numpy as np
 
 def grupos_fisicos(archivo, dim=-1):
     ''' Lee un archivo de texto con extensión .msh que contiene los datos de
@@ -40,7 +39,8 @@ def grupos_fisicos(archivo, dim=-1):
 
     dict1 = dict(zip(tags, nombres))  # Primer diccionario a reportar
 
-    nodos = [gmsh.model.mesh.getNodesForPhysicalGroup(dims[i], tags[i])[0] for i in range(len(dims))]
+    nodos = [gmsh.model.mesh.getNodesForPhysicalGroup(dims[i], tags[i])[0]
+             for i in range(len(dims))]
     dict2 = dict(zip(tags, nodos))  # Segundo diccionario a reportar
 
     gmsh.finalize()
@@ -49,12 +49,14 @@ def grupos_fisicos(archivo, dim=-1):
 
 
 def obtener_nodos(archivo, nombre_grupo):
-    ''' Lee un archivo de extensión .msh y obtiene los nodos asociados al grupo físico de nombre "nombre_grupo".
+    ''' Lee un archivo de extensión .msh y obtiene los nodos asociados al grupo
+        físico de nombre "nombre_grupo".
     '''
     d1, d2 = grupos_fisicos(archivo)
     nombres = [t[1] for t in d1.values()]
     if nombre_grupo in nombres:
         nodos = d2[list(d2.keys())[nombres.index(nombre_grupo)]]
+        gmsh.finalize()
         return nodos
     else:
         raise ValueError('El nombre de grupo físico ingresado no existe.')
