4c6dd0217e7bbbec05d5b81694a46c64d60ff07d
Alejandro Hincapie G <alejohg03@gmail.com>
2020-10-29 23:27:08 -0500

    Actualizando códigos

--- Mallas_GMSH/3_Malla-estructurada-extrude_2d.geo
+++ Mallas_GMSH/3_Malla-estructurada-extrude_2d.geo
@@ -1,30 +1,64 @@
 // Ejemplo 3: Programa para crear una malla estructurada sencilla usando la función 'extrude'.
+// Por: Alejandro Hincapie G.
+
 
 Point(1) = {0, 0, 0}; // Se crea un punto en origen de coordenadas
 
 // Al extruir el punto se genera una línea. El argumento Layers permite definir cuántos elementos
 // queremos a lo largo de esta línea
 
-linea[] = Extrude {0, 20, 0} {
-		  Point{1}; Layers{10};};
+l_ext1[] = Extrude {0, 20, 0} {
+		   Point{1}; Layers{10};};
 
-l1 = linea[1]; // Se extrae la etiqueta de la línea generada al extruir el punto 1
+l1 = l_ext1[1]; // Se extrae la etiqueta de la línea generada al extruir el punto 1
+n = #l_ext1[];  // Número de elementos de la lista generada
 
+Printf("Extrusión punto 1:");
+For i In {0:n-1}
+	Printf("l_ext1[%g] = %g", i, l_ext1[i]);
+EndFor
 
 // Ahora extruimos la línea anterior para generar una superficie. El argumento Recombine permite
 // que en la superficie generada la malla conste de elementos cuadriláteros en lugar de triángulos
 
-superf[] = Extrude {40, 0, 0} {
-		   Curve{l1}; Layers{20}; Recombine;};
+s_ext1[] = Extrude {40, 0, 0} {
+	       Curve{l1}; Layers{20}; Recombine;};
 
-s1 = superf[1];  // Se extrae la etiqueta de la superficie generada al extruir
+Printf(" ");
+Printf("Extrusión línea l1:");
+
+For i In {0:#s_ext1[]-1}
+    Printf("s_ext1[%g] = %g", i, s_ext1[i]);
+EndFor
+
+s1 = s_ext1[1];  // Se extrae la etiqueta de la superficie generada al extruir
+
+Physical Surface("Superficie 1") = {s1};
 
 
-Physical Surface("Mi superficie") = {s1};
+//-------------------------------------------------------------------------------------------------
+// Crear malla usando función Extrude alrededor de un eje de rotación
+//-------------------------------------------------------------------------------------------------
 
-Mesh 2;
+p1 = newp; Point(p1) = {50, 0, 0};
+l_ext2[] = Extrude {15, 0, 0} {Point{p1}; Layers{10};};
+
+l2 = l_ext2[1];
+
+// Ahora usamos la función 'Extrude' para generar la superficie
+
+s_ext2[] = Extrude {{0, 0, 1}, {45, 0, 0}, Pi/2} {
+	       Curve{l2}; Layers{15}; Recombine;};
+
+s2 = s_ext2[1];
+
+Physical Surface("Superficie 2") = {s2};
+
+
+// Ajustes finales de la malla		   
+Mesh 2;                         // Generar la malla 2D
 SetOrder 2;                     // Se definen elementos finitos de orden 2
 Mesh.SecondOrderIncomplete = 1; // Usar elementos finitos serendípitos (incompletos)
 Mesh.SurfaceFaces = 1;          // Ver las "caras" de los elementos finitos 2D
 Mesh.Points = 1;                // Ver los nodos de la malla
-Save "ejm_3.msh";
+Save "extrude.msh";
--- Mallas_python/3_Malla-estructurada-extrude_2d.py
+++ Mallas_python/3_Malla-estructurada-extrude_2d.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Ejemplo 3: Programa para crear una malla estructurada sencilla usando la fun-
-ción 'extrude'.
+Ejemplo 3: Programa para crear una malla estructurada sencilla usando las fun-
+ciones 'extrude' y 'revolve'.
 
 Por: Alejandro Hincapié G.
 """
 
 import gmsh
+from math import pi
 
-# %% Se inicializa el modelo
+
+# Se inicializa el modelo
 gmsh.initialize()
 gmsh.option.setNumber("General.Terminal", 1)
 gmsh.model.add("modelo_3")
@@ -16,13 +18,13 @@ gmsh.model.add("modelo_3")
 # Línea para abreviar el código
 geom = gmsh.model.geo
 
-# %% Se crean las entidades
-p1 = geom.addPoint(0, 0 ,0)
+# Se crean las entidades
+geom.addPoint(0, 0 ,0, tag=1)
 
 # Podemos generar una línea extruyendo el punto creado anteriormente
-line = geom.extrude([(0, p1)], 0, 20, 0, numElements=[10])
+l_ext1 = geom.extrude([(0, 1)], 0, 20, 0, numElements=[10])
 
-print(line)  # Se muestra la lista creada al extruir
+print('\nExtrusión punto 1 = ', l_ext1)  # Se muestra la lista creada al extruir
 
 # =============================================================================
 # En la lista que se crea al extruir una entidad, el segundo elemento siempre
@@ -30,9 +32,9 @@ print(line)  # Se muestra la lista creada al extruir
 # truir un punto (dim=2), la entidad de orden superior que se genera es una linea
 # (dim=1). En la lista creada, cada elemento corresponde a una tupla (dim, tag)
 # =============================================================================
-l1 = line[1]  # Se obtiene la tupla (dim, tag) de la línea recién creada
+l1 = l_ext1[1]  # Se obtiene la tupla (dim, tag) de la línea recién creada
 
-# %% Se crea la superficie extruyendo la línea anterior
+# Se crea la superficie extruyendo la línea anterior
 # =============================================================================
 #     Sintaxis: gmsh.model.geo.extrude([lista de entidades], u, v, w), donde:
 #               - La lista de entidades consiste en pares (dim, tag)
@@ -43,27 +45,46 @@ l1 = line[1]  # Se obtiene la tupla (dim, tag) de la línea recién creada
 #                 genera conste de cuadriláteros en lugar de triángulos
 # =============================================================================
 
-surf = geom.extrude([l1], 40, 0, 0, numElements=[20], recombine=True)
-print(surf)  # Se muestra la lista generada al extruir la línea
+s_ext1 = geom.extrude([l1], 40, 0, 0, numElements=[20], recombine=True)
+print('\nExtrusión curva l1 =', s_ext1, '\n')  # Se muestra la lista generada al extruir la línea
 
-s1 = surf[1]  # Y nuevamente obtenemos su dupla (dim, tag)
+s1 = s_ext1[1]  # Y nuevamente obtenemos su dupla (dim, tag)
 
-# %%% Se crea la superficie física
+# Se crea la superficie física
 gmsh.model.addPhysicalGroup(2, [s1[1]], 21)
-gmsh.model.setPhysicalName(2, 21, 'superficie')
+gmsh.model.setPhysicalName(2, 21, 'superficie 1')
 
-# %% Se sincroniza el modelo y se crea la malla
+# =============================================================================
+# Creamos la segunda malla usando la función 'Revolve'
+# =============================================================================
+p1 = geom.addPoint(50, 0, 0)
+
+# Extruimos el punto para generar una línea
+l_ext2 = geom.extrude([(0, p1)], 15, 0, 0, numElements=[10])
+l2 = l_ext2[1] # Obtenemos tag de la línea generada
+
+# Extruimos esta línea alrededor de un eje de rotación usando la función 'Revolve'
+s_ext2 = geom.revolve([l2], 45, 0, 0, 0, 0, 1, pi/2, numElements=[15], recombine=True)
+s2 = s_ext2[1]
+
+gmsh.model.addPhysicalGroup(2, [s2[1]], 22)
+gmsh.model.setPhysicalName(2, 22, 'superficie 2')
+
+
+# Se sincroniza el modelo y se crea la malla
 geom.synchronize()
 
 gmsh.model.mesh.generate(2)
 
-# Creamos una malla con elementos de orden 2:
+# Creamos una malla con elementos de orden 2
 gmsh.model.mesh.setOrder(2)
 
-# Usar elementos serendípitos (incompletos):
+# Usar elementos serendípitos (incompletos)
 gmsh.option.setNumber('Mesh.SecondOrderIncomplete', 1)
 
-gmsh.option.setNumber('Mesh.SurfaceFaces', 1)  # Para mejorar la visualización
+# Para mejorar la visualización
+gmsh.option.setNumber('Mesh.Points', 1)
+gmsh.option.setNumber('Mesh.SurfaceFaces', 1) 
 
 gmsh.fltk.run()  # Muestra el resultado en la interfaz gráfica
 
